26347dc2b1664933f4c68b0cba3a1bc163b3a1d7
jposluszny <36853153+jposluszny@users.noreply.github.com>
2024-06-19 09:20:27 +0200

    Delete 4 Library directory

--- 4 Library/library/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import books
-from .models import borrowings
-# Register your models here.
-admin.site.register(books)
-admin.site.register(borrowings)
